dcea6027b7ed60c89a151014d442135edab2b454
Philipp Hofmann <hofmann.philipp@gmail.com>
2014-04-05 18:53:16 +0200

    Nailed feature @App can fetch remote repo's states
    Decided to scratch Rugged and shell out so we can speed up development at this point. All Repos passed in are now fetched from remote.

--- weekly-commits-report.rb
+++ weekly-commits-report.rb
@@ -2,6 +2,12 @@
 
 require 'rugged'
 
+# path should be the actual .git path from the repo
+def fetch(path)
+  work_tree = path.gsub(".git", "")
+  system( *%W(git --git-dir #{path} --work-tree #{work_tree} fetch) )
+end
+
 working_dir = ARGV.shift
 
 abort("Must provide a working directory") unless working_dir
@@ -10,13 +16,9 @@ working_glob = File.join(working_dir, "**", ".git")
 
 repo_dirs = Dir.glob(working_glob, File::FNM_DOTMATCH)
 
-repos = repo_dirs.map do |repo_path|
-  Rugged::Repository.new repo_path
+repo_dirs.map do |repo_path|
+  p "successfully fetched #{repo_path}" if fetch repo_path
 end
 
-repos.each do |repo|
-  walker = Rugged::Walker.new(repo)
-  walker.push repo.head.target #push the head's commit sha onto the walker
-  walker.each { |c| puts c.inspect } # inspect all commits from that down
-end
+
 
