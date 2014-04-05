9d8114beb0aa26aa4976f9c6ce8ed7b2d5596f07
Philipp Hofmann <hofmann.philipp@gmail.com>
2014-04-05 18:17:52 +0200

    able to pass in working directory as top of tree
    This enables changing where we pull all the .git dirs from

--- weekly-commits-report.rb
+++ weekly-commits-report.rb
@@ -1,27 +1,22 @@
 #!/usr/bin/env ruby
 
-# use https://github.com/libgit2/rugged
 require 'rugged'
 
-# all dirs under the current path with git repos
-repo_dirs = Dir.glob("**/.git", File::FNM_DOTMATCH)
+working_dir = ARGV.shift
 
-# init rugged objects
+abort("Must provide a working directory") unless working_dir
+
+working_glob = File.join(working_dir, "**", ".git")
+
+repo_dirs = Dir.glob(working_glob, File::FNM_DOTMATCH)
+
 repos = repo_dirs.map do |repo_path|
   Rugged::Repository.new repo_path
 end
 
-# TODO fetch the latest state
-
-
-# inspect all commits
 repos.each do |repo|
   walker = Rugged::Walker.new(repo)
   walker.push repo.head.target #push the head's commit sha onto the walker
   walker.each { |c| puts c.inspect } # inspect all commits from that down
 end
 
-# TODO git log commits on all branches within the last 7 days
-
-# TODO redirect the result into something like an excel sheet /db
-# ex git log --since='last month' --pretty=format:'%h,%an,%ar,%s' > log.csv
